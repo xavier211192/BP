2260f6e01580ac0e1458aa30284558b4887b151e
Prash <xavier211192@gmail.com>
2017-05-06 21:54:26 +0200

    26 April

---
+++ app/src/main/java/com/example/android/bp/activities/Cards.java
@@ -0,0 +1,17 @@
+package com.example.android.bp.activities;
+
+/**
+ * Created by Prashanth on 5/3/2017.
+ */
+
+ class Cards {
+    String name;
+    String age;
+    int photoId;
+
+    Cards(String name, String age, int photoId) {
+        this.name = name;
+        this.age = age;
+        this.photoId = photoId;
+    }
+}
