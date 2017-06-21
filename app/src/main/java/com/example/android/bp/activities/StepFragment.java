4f029d535913c299c8eb9824cb50149bf30d79bd
Prash <xavier211192@gmail.com>
2017-06-22 01:16:53 +0200

    26 April

--- app/src/main/java/com/example/android/bp/activities/StepFragment.java
+++ app/src/main/java/com/example/android/bp/activities/StepFragment.java
@@ -4,9 +4,12 @@ package com.example.android.bp.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +26,11 @@ import android.view.LayoutInflater;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
@@ -49,23 +57,30 @@ import com.google.android.gms.fitness.result.DailyTotalResult;
 import com.google.android.gms.fitness.result.DataReadResult;
 
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 import com.example.android.bp.R;
 import com.jjoe64.graphview.series.BarGraphSeries;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StepFragment extends Fragment implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener,TextToSpeech.OnInitListener
 {
 
+
+    private String textToSpeech;
     BarChart chart;
     public ArrayList<BarEntry> entries = new ArrayList<>();
     public ArrayList<String> labels = new ArrayList<String>();
@@ -73,9 +88,30 @@ public class StepFragment extends Fragment implements
     public  int[] weekStep = new int[10];
     public  String[] date = new String[10];
 //    public int weekStep = 0;
+    TextView totalTextView;
+    TextView averageTextView;
+    TextView averageDistanceView;
+    TextView todayDistanceView;
+    TextView todayTextView;
+    ProgressBar progressBar ;
+    ImageView trendup;
+    ImageView trenddown;
+    ImageButton speakEnglish;
+    ImageButton speakPolish;
+
+    public String todayCount;
+    public String todayDistance;
+    public String averageSteps;
     public int total = 0;
-
-
+    public int today = 0;
+    public int averageC = 0;
+    public int yesterday = 0;
+    public double distanceToday = 0;
+    public double distanceAvg = 0;
+    private TextToSpeech myTTS;
+    private String englishTTS;
+    private String polishTTS;
+    private int MY_DATA_CHECK_CODE = 0;
     public GoogleApiClient mGoogleApiClient;
 
     public static GoogleApiClient mGoogleApiClient1;
@@ -97,6 +133,21 @@ public class StepFragment extends Fragment implements
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_step, container, false);
         chart =  (BarChart) v.findViewById(R.id.chartx);
+        todayTextView = (TextView) v.findViewById(R.id.todayStepsText);
+        averageTextView = (TextView) v.findViewById(R.id.averageStepsTextView);
+        totalTextView = (TextView) v.findViewById(R.id.totalStepsTextView);
+        progressBar = (ProgressBar) v.findViewById(R.id.cPBS);
+        trenddown = (ImageView) v.findViewById(R.id.trenddownsteps);
+        trendup = (ImageView) v.findViewById(R.id.trendupsteps);
+        trendup.setVisibility(View.INVISIBLE);
+        trenddown.setVisibility(View.INVISIBLE);
+        averageDistanceView = (TextView) v.findViewById(R.id.avgdistanceView);
+        todayDistanceView = (TextView) v.findViewById(R.id.todayDistanceView);
+        speakEnglish = (ImageButton) v.findViewById(R.id.speakEnglish);
+        speakEnglish.setOnClickListener(this);
+        speakPolish = (ImageButton) v.findViewById(R.id.speakPolish);
+        speakPolish.setOnClickListener(this);
+
         return v;
     }
 
@@ -104,6 +155,9 @@ public class StepFragment extends Fragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Context cont = getContext();
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent,MY_DATA_CHECK_CODE);
         mGoogleApiClient = new GoogleApiClient.Builder(cont)
                 .addApi(Fitness.HISTORY_API)
                 .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
@@ -112,6 +166,28 @@ public class StepFragment extends Fragment implements
                 .build();
         mGoogleApiClient1 =mGoogleApiClient;
        new ViewWeekStepCountTask().execute();
+    }
+
+    public void onInit(int initStatus){
+        if(initStatus == TextToSpeech.SUCCESS){
+            myTTS.setLanguage(Locale.UK);
+        }else if (initStatus == TextToSpeech.ERROR) {
+            Toast.makeText(getActivity(), "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // onActivityResult method for text to speech
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                myTTS = new TextToSpeech(getActivity(), this);
+            }
+            else {
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
     }
 
     @Override
@@ -221,9 +297,11 @@ public class StepFragment extends Fragment implements
             }
 
             BarDataSet dataset = new BarDataSet(entries, "No of Steps");
+            dataset.setBarSpacePercent(75f);
             BarData data = new BarData(labels, dataset);
 
-            dataset.setColors(ColorTemplate.LIBERTY_COLORS);
+//            dataset.setColors(ColorTemplate.LIBERTY_COLORS);
+            dataset.setColor(Color.parseColor("#9575cd"));
             chart.setDrawGridBackground(false);
             chart.getXAxis().setDrawGridLines(false);
             chart.getAxisLeft().setDrawGridLines(false);
@@ -241,22 +319,46 @@ public class StepFragment extends Fragment implements
             //
 //                    chart.notifyDataSetChanged();
 //                    progressBar.setMax(80000);
-            for(int i :weekStep){
-                total = total + i;
+            for(int i=0; i<8; i++){
+                if(i==6){
+                    yesterday = weekStep[i];
+                }
+                if(i==7){
+                    today = weekStep[i];
+                }
+                total = total + weekStep[i];
             }
-//                    progressBar.setProgress(total);
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//
-//                            week_total_text.setText(String.valueOf(total));
-//                            week_total_text.append(" Steps");
-//
-//                        }
-//                    });
 
+            averageC = total/8;
+            averageSteps = String.valueOf(averageC);
+            todayCount = String.valueOf(today);
+            distanceAvg = averageC*0.000762;
+            distanceToday = Math.floor(today*0.0007620);
+            todayDistance = String.valueOf(distanceToday);
+            progressBar.setMax(80000);
+            progressBar.setProgress(total);
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    totalTextView.setText(String.valueOf(total));
+                    averageTextView.setText(String.valueOf(averageC + " steps"));
+                    todayTextView.setText(String.valueOf(today + " steps"));
+                    if(today > yesterday){
+                        trendup.setVisibility(View.VISIBLE);
+                    }else trenddown.setVisibility(View.VISIBLE);
+                    todayDistanceView.setText(new DecimalFormat("##.##").format(distanceToday)+ " km");
+                    averageDistanceView.setText(new DecimalFormat("##.##").format(distanceAvg) + " km");
+
+
+                }
+            });
+//            Intent i = new Intent(getActivity(),CombinedFragment.class);
+//            i.putExtra("numbers",weekStep);
+//            startActivity(i);
 
         }
+
 
 //
 
@@ -272,11 +374,44 @@ public class StepFragment extends Fragment implements
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.speakEnglish: {
+                myTTS.setLanguage(Locale.UK);
+                englishTTS = " Hi, Your step count for today is:" + todayCount+ "and you have travelled," + todayDistance+",kilometres. You average," + averageSteps + ",steps,this week";
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    myTTS.speak(englishTTS, TextToSpeech.QUEUE_FLUSH, null, null);
+                }else {
+                    myTTS.speak(englishTTS,TextToSpeech.QUEUE_FLUSH,null);
+                }
+            }
+            break;
+            case R.id.speakPolish: {
+                myTTS.setLanguage(new Locale("pl_PL"));
+                polishTTS = " CZEŚĆ, Twój krok liczą się na dziś:" + todayCount+ ",i podróżowałeś,"+ todayDistance+",kilometry. Średnio," +averageSteps+" kroków w tym tygodniu" ;
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    myTTS.speak(polishTTS, TextToSpeech.QUEUE_FLUSH, null, null);
+                }else {
+                    myTTS.speak(polishTTS,TextToSpeech.QUEUE_FLUSH,null);
+                }
+            }
+            break;
+
+        }
+
+
+    }
 
 
     private class ViewWeekStepCountTask extends AsyncTask<Void, Void, Void> {
         protected Void doInBackground(Void... params) {
             displayLastWeeksStepsData();
+
 
             return null;
         }
